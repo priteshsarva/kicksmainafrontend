f75ae56ea0b62e091124f1accd6d9203f54f1d6e
pritesh sarva <priteshsarva9825@gmail.com>
2025-03-06 23:26:10 +0530

    new1

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,4 @@ export default defineConfig({
   plugins: [react()],
   base: '/kicksmainafrontend/',
   // outDir: 'build',
-<<<<<<< HEAD
 })
-=======
-})
->>>>>>> be652c1a784a72e9ee909878fdd9c519ba4ba1cc
